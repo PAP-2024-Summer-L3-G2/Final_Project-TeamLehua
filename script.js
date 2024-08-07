4909c322fc68118b572772e56b7cf74192c6ef94
Eda Keskin <edake2007@gmail.com>
2024-08-07 13:42:41 -0700

    Shop filter button added

--- script.js
+++ script.js
@@ -21,21 +21,27 @@ const honeysSold = [
         "Name": "Honey Bee Raw Clover Honey",
         "Company": "Go Raw Honey",
         "Size (oz)": 24,
-        "Price ($)": 24.99
+        "Price ($)": 24.99,
+        img: "images/Water_White_Honey-removebg-preview.png",
+        href: "https://www.gorawhoney.com/product/baby-bee-12oz-pure-raw-clover-honey/"
     },
     {
         "Color": "extra-light-amber",
         "Name": "Queen Bee Raw Clover Honey",
         "Company": "Go Raw Honey",
         "Size (oz)": 48,
-        "Price ($)": 35.99
+        "Price ($)": 35.99,
+        img: "images/rawclove-removebg-preview.png",
+        href: "https://www.gorawhoney.com/product-category/raw-clover-honey/"
     },
     {
         "Color": "extra-light-amber",
         "Name": "Whole Hive Bulk Raw Honey",
         "Company": "Go Raw Honey",
         "Size (oz)": 192,
-        "Price ($)": 76.99
+        "Price ($)": 76.99,
+        img: "images/bulk-raw-honey.png",
+        href: "https://www.gorawhoney.com/product/whole-hive-1-gallon-pure-raw-clover-honey/"
     },
     // https://goldengirlshoneyandhives.com/product/fireweed-honey/
     {
@@ -43,7 +49,9 @@ const honeysSold = [
         "Name": "Pacific Northwest Fireweed Honey",
         "Company": "Golden Girls Honey",
         "Size (oz)": 13,
-        "Price ($)": 16
+        "Price ($)": 16,
+        img: "images/fireweed-removebg-preview.png",
+        href: "https://goldengirlshoneyandhives.com/product/fireweed-honey/"
     }, 
     // https://www.gorawhoney.com/product/wildflower-varietal-raw-honey-queen-bee-3lb/
     {
@@ -51,7 +59,9 @@ const honeysSold = [
         "Name": "Wildflower Varietal Raw Honey Queen Bee",
         "Company": "Go Raw Honey",
         "Size (oz)": 48,
-        "Price ($)": 37.99
+        "Price ($)": 37.99,
+        img: "images/Wildflower-QueenBee-800px-COMP-removebg-preview.png",
+        href: "https://www.gorawhoney.com/product/wildflower-varietal-raw-honey-queen-bee-3lb/"
     },
     // https://www.smileyhoney.com/products/sourwood-honey
     {
@@ -59,21 +69,27 @@ const honeysSold = [
         "Name": "Sourwood Honey",
         "Company": "Smiley Honey LLC",
         "Size (oz)": 15,
-        "Price ($)": 16.5
+        "Price ($)": 16.5,
+        img: "images/15-oz-sourwood-honey.png",
+        href: "https://www.smileyhoney.com/products/sourwood-honey",
     },
     {
         "Color": "light-amber",
         "Name": "Sourwood Honey",
         "Company": "Smiley Honey LLC",
         "Size (oz)": 16,
-        "Price ($)": 16
+        "Price ($)": 16,
+        img: "images/16-oz-sour-wood-honey.png",
+        href: "https://www.smileyhoney.com/products/sourwood-honey",
     },
     {
         "Color": "light-amber",
         "Name": "Sourwood Honey",
         "Company": "Smiley Honey LLC",
         "Size (oz)": 8,
-        "Price ($)": 8
+        "Price ($)": 8,
+        img: "",
+        href: "",
     },
     // https://www.gorawhoney.com/product/baby-bee-12oz-pure-raw-clover-honey/
     {
@@ -81,7 +97,9 @@ const honeysSold = [
         "Name": "Baby Bee Raw Clover Honey",
         "Company": "Go Raw Honey",
         "Size (oz)": 12,
-        "Price ($)": 19.99
+        "Price ($)": 19.99,
+        img: "",
+        href: "",
     },
 ];
 
@@ -112,3 +130,4 @@ function textSortBy(type, biggerAtTop) {
         })
     }
 }
+//
